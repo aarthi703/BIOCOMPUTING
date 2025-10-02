62f6994cd59480c9f1bece53424813d77bb13ad8
aarthi703 <abharathan01@wm.edu>
2025-10-01 23:48:03 -0400

    add assignment_5

--- assignments/assignment_5/pipeline.sh
+++ assignments/assignment_5/pipeline.sh
@@ -2,11 +2,8 @@
 
 set -euo pipefail
 MAIN_DIR=${HOME}/BIOCOMPUTING/assignments/assignment_5
-SCRIPTS_DIR=${MAIN_DIR}/scripts
 
-#go to data directory for assignment_5
 
-#cd ${DATA_DIR} 
 
 #run ./scripts/01_download_data.sh
 
